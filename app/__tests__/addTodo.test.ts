34be55b4ecfbb8a187b7f39b19db2f98644e1a52
Owale128 <Christian.buaka@hotmail.com>
2025-11-16 21:33:44 +0100

    test: Add unit test for addTodo function (TDD)

---
+++ app/__tests__/addTodo.test.ts
@@ -0,0 +1,17 @@
+import { Todo } from "../models/Todo";
+import { addTodo } from "../utils/todoutils";
+
+describe('addTodo', () => {
+  it('should add a new todo item to the list', () => {
+    const todos: Todo[] = [];
+    const result = addTodo( todos, 'New Todo')
+
+    expect(result).toHaveLength(1)
+
+    expect(result[0]).toMatchObject({
+        id: 1,
+        text:'New Todo',
+        completed: false
+    })
+  });
+})
