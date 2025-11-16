5f322ced101d82798358aca28f307eec924a9903
Owale128 <Christian.buaka@hotmail.com>
2025-11-16 22:08:27 +0100

    feat: Add logic to not add an empty todo item in addTodo function

--- app/utils/todoutils.ts
+++ app/utils/todoutils.ts
@@ -4,6 +4,10 @@ export function addTodo(todos: Todo[], text: string): Todo[] {
 
     text = text.trim();
 
+    if(text === "") {
+        return todos
+    }
+
     const newTodo: Todo = {
         id: todos.length + 1,
         text,
