76e5cd557570c78dc1dd0a9486fd0f1633dc959d
Owale128 <Christian.buaka@hotmail.com>
2025-11-17 13:33:25 +0100

    test(removeTodo): Add testfall for eadge case

--- app/__tests__/removeTodo.test.ts
+++ app/__tests__/removeTodo.test.ts
@@ -8,7 +8,6 @@ describe("removeTodo", () => {
       { id: 2, text: "Second todo", done: false },
       { id: 3, text: "Third todo", done: false },
     ];
-
     const result = deleteTodo(todos, 1);
 
     expect(result).toEqual([
@@ -17,5 +16,14 @@ describe("removeTodo", () => {
     ]);
     expect(result).toHaveLength(2);
   });
-  
+
+  it("should leave the list unchaged if id does not exist ", () => {
+    const todos: Todo[] = [
+      { id: 1, text: "First todo", done: false },
+      { id: 2, text: "Second todo", done: false },
+    ];
+    const result = deleteTodo(todos, 999);
+
+    expect(result).toEqual(todos);
+  });
 });
