c2124cc17195fc087fdfc584faa228ab424509ad
Owale128 <Christian.buaka@hotmail.com>
2025-11-17 11:15:49 +0100

    feat: Implement TodoApp component with addTodo functionality

---
+++ app/components/TodoApp.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { TodosContextValue } from "../models/TodosContextValue";
+import { Todo } from "../models/Todo";
+import { addTodo } from "../utils/todoutils";
+
+const TodoApp = () => {
+    const [todos, setTodos] = useState<Todo[]>([]) 
+
+    const handleAdd: TodosContextValue['add'] = (text) => {
+        setTodos((prev) => addTodo(prev, text))
+    }
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TodoApp
