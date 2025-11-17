4fe0eeb2b1ceb0df046d7b332f7f4e061c025d05
Owale128 <Christian.buaka@hotmail.com>
2025-11-17 13:23:36 +0100

    test(removeTodo): Add unit test for deleteTodo function

---
+++ app/__tests__/removeTodo.test.ts
@@ -0,0 +1,21 @@
+import { Todo } from "../models/Todo";
+import { deleteTodo } from "../utils/todoUtilities";
+
+describe("removeTodo", () => {
+  it("should remove todo with correct id", () => {
+    const todos: Todo[] = [
+      { id: 1, text: "First todo", done: false },
+      { id: 2, text: "Second todo", done: false },
+      { id: 3, text: "Third todo", done: false },
+    ];
+
+    const result = deleteTodo(todos, 1);
+
+    expect(result).toEqual([
+      { id: 2, text: "Second todo", done: false },
+      { id: 3, text: "Third todo", done: false },
+    ]);
+    expect(result).toHaveLength(2);
+  });
+  
+});
