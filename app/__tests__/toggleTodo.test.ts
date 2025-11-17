026e5422bbe74368aac47a54ceb6a3a298a5f421
Owale128 <Christian.buaka@hotmail.com>
2025-11-17 13:49:43 +0100

    fix(toggleTodo): Format code with Prettier

--- app/__tests__/toggleTodo.test.ts
+++ app/__tests__/toggleTodo.test.ts
@@ -7,18 +7,16 @@ describe("toggleTodo", () => {
       { id: 1, text: "First todo", done: false },
       { id: 2, text: "Second todo", done: false },
     ];
-
     const result = toggleTodo(todos, 2);
+
     expect(result[0].done).toBe(false);
     expect(result[1].done).toBe(true);
   });
 
-    it("should not change todos if id not found", () => {
-        const todos: Todo[] = [
-            {id: 1, text: "My todo", done: false}
-        ]
-        
-        const result = toggleTodo(todos, 999)
-        expect(result).toEqual(todos)
-     })
+  it("should not change todos if id not found", () => {
+    const todos: Todo[] = [{ id: 1, text: "My todo", done: false }];
+    const result = toggleTodo(todos, 999);
+
+    expect(result).toEqual(todos);
+  });
 });
