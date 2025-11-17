f378f36380f6653c147033ee6ea8e41a5aa6f2e0
Owale128 <Christian.buaka@hotmail.com>
2025-11-17 13:45:18 +0100

    feat(todoUtilities): Change name from deleteTodo to removeTodo and add logic in the function

--- app/utils/todoUtilities.ts
+++ app/utils/todoUtilities.ts
@@ -1,25 +1,26 @@
 import { Todo } from "../models/Todo";
 
-export const addTodo =(todos: Todo[], text: string): Todo[] => {
+export const addTodo = (todos: Todo[], text: string): Todo[] => {
+  text = text.trim();
 
-    text = text.trim();
+  if (text === "") {
+    return todos;
+  }
 
-    if(text === "") {
-        return todos
-    }
-
-    const newTodo: Todo = {
-        id: todos.length + 1,
-        text,
-        done: false
-    }
-    return [...todos, newTodo];
-}
+  const newTodo: Todo = {
+    id: todos.length + 1,
+    text,
+    done: false,
+  };
+  return [...todos, newTodo];
+};
 
 export const toggleTodo = (todo: Todo[], id: number): Todo[] => {
-    return todo.map(todo => todo.id === id? {...todo, done: !todo.done}: todo)
-}
+  return todo.map((todo) =>
+    todo.id === id ? { ...todo, done: !todo.done } : todo
+  );
+};
 
-export const deleteTodo = (todos: Todo[], id: number): Todo[] => {
-    throw new Error("Not implemented yet")
-}
+export const removeTodo = (todos: Todo[], id: number): Todo[] => {
+  return todos.filter((todo) => todo.id !== id);
+};
