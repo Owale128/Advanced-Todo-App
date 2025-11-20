33990f48e76fab74c41a10541f931fef9810e75c
Owale128 <Christian.buaka@hotmail.com>
2025-11-20 21:09:51 +0100

    feat: implement login form with validation and routing

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,83 @@
-import TodoApp from "./components/TodoApp";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { loginSchema } from "./lib/schemas/auth";
+import z from "zod";
+import Link from "next/link";
 
 export default function Home() {
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof loginSchema>) => {
+    console.log(values);
+    router.push("/todo-app");
+  };
+
   return (
-    <main className="flex items-center justify-center min-h-screen p-8">
-      <div className="w-full max-w-2xl bg-white rounded-lg shadow-2xl p-12">
-        <TodoApp />
-      </div>
+    <main className="flex flex-col items-center justify-center min-h-screen p-8 gap-10">
+      <h1 className="text-5xl tracking-wide">Login page</h1>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="border p-8 rounded-lg space-y-6 w-96"
+        >
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-lg">Namn</FormLabel>
+                <FormControl>
+                  <Input placeholder="" type="text" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-lg">Lösenord</FormLabel>
+                <FormControl>
+                  <Input placeholder="" type="password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="w-full cursor-pointer text-base">
+            Logga in
+          </Button>
+          <div className="flex gap-2 justify-center ">
+            <p>Inget konto? </p>
+            <Link href="/register" className="text-blue-500 underline">
+              Registrera dig här
+            </Link>
+          </div>
+        </form>
+      </Form>
     </main>
   );
 }
