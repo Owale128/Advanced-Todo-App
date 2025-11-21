317d109bb5cb8aaa2458889d571ae7352e522391
Owale128 <Christian.buaka@hotmail.com>
2025-11-21 20:50:29 +0100

    feat(lib): Add register schema

---
+++ lib/schemas/auth.ts
@@ -0,0 +1,27 @@
+import z from "zod";
+
+export const loginSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters.",
+  }),
+});
+
+export const registerSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters.",
+  }),
+  confirmPassword: z.string().min(6, {
+    message: "Password must be at least 6 characters.",
+  }),
+}).refine((data) => data.password === data.confirmPassword, {
+  message: "Passwords do not match.",
+  path: ["confirmPassword"],
+});
