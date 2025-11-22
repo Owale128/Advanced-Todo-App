2afa7ed9deb6829a76e80876b6525d475d148a2d
Owale128 <Christian.buaka@hotmail.com>
2025-11-22 23:17:41 +0100

    fix(components): move AddTodo component in addTodo folder

---
+++ app/components/addTodo/AddTodo.tsx
@@ -0,0 +1,26 @@
+import { TodosContext } from "@/app/context/TodoContext";
+import { FormEvent, useContext, useState } from "react";
+import AddTodoView from "./AddTodoView";
+2;
+const AddTodo = () => {
+  const [inputValue, setInputValue] = useState("");
+  const { add } = useContext(TodosContext);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    add(inputValue);
+    setInputValue("");
+  };
+
+  return (
+    <section>
+      <AddTodoView
+      handleSubmit={handleSubmit}
+        inputValue={inputValue}
+        setInputValue={setInputValue}
+      />
+    </section>
+  );
+};
+
+export default AddTodo;
