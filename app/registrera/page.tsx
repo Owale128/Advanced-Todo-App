6c032914092c4af07655e83313e532a3bddfa3cb
Owale128 <Christian.buaka@hotmail.com>
2025-11-23 20:49:32 +0100

    refactor: simplify Register component by using RegisterView

--- app/registrera/page.tsx
+++ app/registrera/page.tsx
@@ -1,21 +1,10 @@
 "use client";
-
-import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useRouter } from "next/navigation";
-import { useForm } from "react-hook-form";
 import z from "zod";
-import Link from "next/link";
+import { useForm } from "react-hook-form";
+import RegisterView from "./RegisterView";
+import { useRouter } from "next/navigation";
 import { registerSchema } from "@/lib/schemas/auth";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const Register = () => {
   const router = useRouter();
@@ -37,62 +26,7 @@ const Register = () => {
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-4 gap-10">
-      <h1 className="text-5xl tracking-wide">Registrera</h1>
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="border p-8 rounded-lg space-y-6 w-full max-w-sm shadow-2xl bg-white"
-        >
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel className="text-lg">Namn</FormLabel>
-                <FormControl>
-                  <Input placeholder="" type="text" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel className="text-lg">Lösenord</FormLabel>
-                <FormControl>
-                  <Input placeholder="" type="password" {...field} />
-                </FormControl> 
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="confirmPassword"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel className="text-lg">Bekräfta lösenord</FormLabel>
-                <FormControl>
-                  <Input placeholder="" type="password" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit" className="w-full cursor-pointer text-base">
-            Registrera konto
-          </Button>
-          <div className="flex gap-2 justify-center ">
-            <p>Har redan ett konto? </p>
-            <Link href="/" className="text-blue-500 underline cursor-pointer">
-              Logga in
-            </Link>
-          </div>
-        </form>
-      </Form>
+      <RegisterView form={form} onSubmit={onSubmit}/>
     </main>
   );
 }
