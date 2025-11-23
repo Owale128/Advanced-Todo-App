d6d152b95875c097210008ce26bd2e3e124bb054
Owale128 <Christian.buaka@hotmail.com>
2025-11-23 01:56:44 +0100

    feat(components): implement HomeView component for login form

---
+++ app/components/HomeView.tsx
@@ -0,0 +1,64 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { HomeViewProps } from "../models/HomeViewProps";
+
+const HomeView = ({ form, onSubmit }: HomeViewProps) => {
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="border p-8 rounded-lg space-y-6 w-full max-w-sm shadow-2xl bg-white"
+      >
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-lg">Namn</FormLabel>
+              <FormControl>
+                <Input placeholder="" type="text" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-lg">Lösenord</FormLabel>
+              <FormControl>
+                <Input placeholder="" type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-full cursor-pointer text-base">
+          Logga in
+        </Button>
+        <div className="flex gap-2 justify-center ">
+          <p>Inget konto? </p>
+          <Link
+            href="/registrera"
+            className="text-blue-500 underline cursor-pointer"
+          >
+            Registrera dig här
+          </Link>
+        </div>
+      </form>
+    </Form>
+  );
+};
+
+export default HomeView;
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,10 @@
 "use client";
-
-import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useRouter } from "next/navigation";
-import { useForm } from "react-hook-form";
-import { loginSchema } from "../lib/schemas/auth";
 import z from "zod";
-import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import HomeView from "./components/HomeView";
+import { loginSchema } from "../lib/schemas/auth";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export default function Home() {
   const router = useRouter();
@@ -36,48 +25,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-4 gap-10 ">
       <h1 className="text-5xl tracking-wide">Login</h1>
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="border p-8 rounded-lg space-y-6 w-full max-w-sm shadow-2xl"
-        >
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel className="text-lg">Namn</FormLabel>
-                <FormControl>
-                  <Input placeholder="" type="text" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel className="text-lg">Lösenord</FormLabel>
-                <FormControl>
-                  <Input placeholder="" type="password" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit" className="w-full cursor-pointer text-base">
-            Logga in
-          </Button>
-          <div className="flex gap-2 justify-center ">
-            <p>Inget konto? </p>
-            <Link href="/registrera" className="text-blue-500 underline">
-              Registrera dig här
-            </Link>
-          </div>
-        </form>
-      </Form>
+      <HomeView form={form} onSubmit={onSubmit} />
     </main>
   );
 }
