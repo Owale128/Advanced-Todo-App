1448e3b15222a27666837383f3513568de3a4389
Owale128 <Christian.buaka@hotmail.com>
2025-11-29 11:52:17 +0100

    feat: add api call to register route

--- app/registrera/page.tsx
+++ app/registrera/page.tsx
@@ -5,6 +5,7 @@ import RegisterView from "./RegisterView";
 import { useRouter } from "next/navigation";
 import { registerSchema } from "@/lib/schemas/auth";
 import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
 
 const Register = () => {
   const router = useRouter();
@@ -18,18 +19,31 @@ const Register = () => {
     },
   });
 
-  const onSubmit = (values: z.infer<typeof registerSchema>) => {
-    console.log(values);
-    alert("Registrering lyckades! Vänligen logga in.");
-    router.push("/");
+  const onSubmit = async (values: z.infer<typeof registerSchema>) => {
+    try {
+      const response = await axios.post("/api/auth/register", {
+        username: values.username,
+        password: values.password,
+      });
+
+      if (response.status === 200) {
+        router.push("/");
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response?.data?.error) {
+        alert(error.response.data.error)
+      } else {
+        alert("Registrering misslyckades. Var vänlig försök igen!");
+      }
+    }
   };
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-4 gap-10 auth-background">
       <h1 className="text-5xl tracking-wide text-gray-800">Registrera</h1>
-      <RegisterView form={form} onSubmit={onSubmit}/>
+      <RegisterView form={form} onSubmit={onSubmit} />
     </main>
   );
-}
+};
 
 export default Register;
