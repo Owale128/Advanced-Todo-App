99e8bb0f671e005fd007a1b0144c827b30abc6ff
Owale128 <Christian.buaka@hotmail.com>
2025-11-29 11:50:34 +0100

    feat: add api call to login route

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 import HomeView from "./components/HomeView";
 import { loginSchema } from "../lib/schemas/auth";
 import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
 
 export default function Home() {
   const router = useRouter();
@@ -17,9 +18,23 @@ export default function Home() {
     },
   });
 
-  const onSubmit = (values: z.infer<typeof loginSchema>) => {
-    console.log(values);
-    router.push("/todo-app");
+  const onSubmit = async (values: z.infer<typeof loginSchema>) => {
+    try {
+      const response = await axios.post("/api/auth/login", {
+        username: values.username,
+        password: values.password,
+      });
+
+      if (response.status === 200) {
+        router.push("/todo-app");
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response?.data?.error) {
+        alert(error.response.data.error);
+      } else {
+        alert("Inloggning misslyckades. Var vänlig försök igen!");
+      }
+    }
   };
 
   return (
