39e2e43f23107991c32a8c551374d6c743561b96
Owale128 <Christian.buaka@hotmail.com>
2025-12-06 16:46:40 +0100

    feat(todoUtilities): update addTodo, toggleTodo, and removeTodo functions to use _id and handle order, priority, category, and dueDate.

--- app/utils/todoUtilities.ts
+++ app/utils/todoUtilities.ts
@@ -7,20 +7,27 @@ export const addTodo = (todos: Todo[], text: string): Todo[] => {
     return todos;
   }
 
+  const maxOrder = todos.length > 0 ? Math.max(...todos.map(t => t.order)) : -1;
+  const newOrder = maxOrder + 1;
+
   const newTodo: Todo = {
-    id: todos.length + 1,
+    _id: String(todos.length + 1),
     text,
     done: false,
+    order: newOrder,
+    priority: "medium",
+    category: "General",
+    dueDate: null,
   };
   return [...todos, newTodo];
 };
 
-export const toggleTodo = (todo: Todo[], id: number): Todo[] => {
+export const toggleTodo = (todo: Todo[], id: string): Todo[] => {
   return todo.map((todo) =>
-    todo.id === id ? { ...todo, done: !todo.done } : todo
+    todo._id === id ? { ...todo, done: !todo.done } : todo
   );
 };
 
-export const removeTodo = (todos: Todo[], id: number): Todo[] => {
-  return todos.filter((todo) => todo.id !== id);
+export const removeTodo = (todos: Todo[], id: string): Todo[] => {
+  return todos.filter((todo) => todo._id !== id);
 };
