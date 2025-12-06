304d078f2ff79f27025f42f6a8e80d90a9552eb7
Owale128 <Christian.buaka@hotmail.com>
2025-12-06 17:37:29 +0100

    chore(models): remove UserSchema from User file

--- app/models/User.ts
+++ app/models/User.ts
@@ -1,27 +1,10 @@
-import mongoose, { Schema, Document } from "mongoose";
+import { UserSchema } from "@/lib/schemas/user";
+import mongoose, { Document } from "mongoose";
 
-export interface IUser extends Document {
+export interface User extends Document {
   username: string;
   password: string;
   createdAt: Date;
 }
 
-const UserSchema = new Schema<IUser>({
-  username: {
-    type: String,
-    required: [true, "Username is required"],
-    unique: true,
-    minlength: [2, "Username must be at least 2 characters"],
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-    minlength: [6, "Password must be at least 6 characters"],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
-
-export default mongoose.models.User || mongoose.model<IUser>("User", UserSchema);
+export default mongoose.models.User || mongoose.model<User>("User", UserSchema);
