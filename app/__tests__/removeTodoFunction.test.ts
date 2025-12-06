db9420a655dd515d553947c07c42e392b6f9cbdd
Owale128 <Christian.buaka@hotmail.com>
2025-12-06 20:00:07 +0100

    test(removeTodoFunction): update tests to use correct Todo structure and string id

--- app/__tests__/removeTodoFunction.test.ts
+++ app/__tests__/removeTodoFunction.test.ts
@@ -4,25 +4,55 @@ import { removeTodo } from "../utils/todoUtilities";
 describe("removeTodo", () => {
   it("should remove todo with correct id", () => {
     const todos: Todo[] = [
-      { id: 1, text: "First todo", done: false },
-      { id: 2, text: "Second todo", done: false },
-      { id: 3, text: "Third todo", done: false },
+      {
+        _id: "1", text: "First todo", done: false,
+        order: 0,
+        priority: "low",
+        category: "",
+        dueDate: null
+      },
+      {
+        _id: "2", text: "Second todo", done: false,
+        order: 0,
+        priority: "low",
+        category: "",
+        dueDate: null
+      },
+      {
+        _id: "3", text: "Third todo", done: false,
+        order: 0,
+        priority: "low",
+        category: "",
+        dueDate: null
+      },
     ];
-    const result = removeTodo(todos, 1);
+    const result = removeTodo(todos, "1");
 
     expect(result).toEqual([
-      { id: 2, text: "Second todo", done: false },
-      { id: 3, text: "Third todo", done: false },
+      { _id: "2", text: "Second todo", done: false },
+      { _id: "3", text: "Third todo", done: false },
     ]);
     expect(result).toHaveLength(2);
   });
 
   it("should leave the list unchaged if id does not exist ", () => {
     const todos: Todo[] = [
-      { id: 1, text: "First todo", done: false },
-      { id: 2, text: "Second todo", done: false },
+      {
+        _id: "1", text: "First todo", done: false,
+        order: 0,
+        priority: "low",
+        category: "",
+        dueDate: null
+      },
+      {
+        _id: "2", text: "Second todo", done: false,
+        order: 0,
+        priority: "low",
+        category: "",
+        dueDate: null
+      },
     ];
-    const result = removeTodo(todos, 999);
+    const result = removeTodo(todos, "999");
 
     expect(result).toEqual(todos);
     expect(result).toHaveLength(2);
