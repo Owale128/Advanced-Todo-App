8589a89acffb782191bafb827b82c92ab9812e51
Owale128 <Christian.buaka@hotmail.com>
2025-12-06 18:41:48 +0100

    test(HomeComponent): mock axios and improve test structure

--- app/__tests__/HomeComponent.test.tsx
+++ app/__tests__/HomeComponent.test.tsx
@@ -1,21 +1,31 @@
 import Home from "../page";
 import { useRouter } from "next/navigation";
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import axios from "axios";
 
 jest.mock("next/navigation", () => ({
   useRouter: jest.fn(),
 }));
+
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe("Home Page", () => {
   const mockPush = jest.fn();
 
   beforeEach(() => {
     (useRouter as jest.Mock).mockReturnValue({ push: mockPush });
+
+    mockedAxios.post.mockResolvedValue({
+      status: 200,
+      data: { message: "Login successful" },
+    });
   });
 
   afterEach(() => {
     mockPush.mockClear();
-  })
+    jest.clearAllMocks;
+  });
 
   it("should navigate to /todo-app on successful login", async () => {
     render(<Home />);
@@ -40,7 +50,9 @@ describe("Home Page", () => {
     fireEvent.click(submitButton);
 
     await waitFor(() => {
-      expect(screen.getByText(/Username must be at least 2 characters./i)).toBeInTheDocument();
+      expect(
+        screen.getByText(/Username must be at least 2 characters./i)
+      ).toBeInTheDocument();
     });
     expect(mockPush).not.toHaveBeenCalled();
   });
