be448d89be894e17e5fd7c7e0c998a3f13ef826f
Owale128 <Christian.buaka@hotmail.com>
2025-12-06 22:42:40 +0100

    style(Header): update margin styles

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -63,11 +63,11 @@ const Header = () => {
         </Button>
       </div>
       {username && (
-        <p className="text-sm md:text-base text-gray-600 dark:text-gray-400 uppercase tracking-wider mt-8 mb-2">
+        <p className="text-sm md:text-base text-gray-600 dark:text-gray-400 uppercase tracking-wider mt-16 md:mt-8 mb-2">
           Inloggad som: <span className="font-semibold text-gray-800 dark:text-gray-200">{username}</span>
         </p>
       )}
-      <h1 className="text-3xl md:text-5xl tracking-widest my-12 font-(family-name:--font-playfair) uppercase dark:text-gray-100">
+      <h1 className="text-3xl md:text-5xl tracking-widest my-8 md:my-12 font-(family-name:--font-playfair) uppercase dark:text-gray-100">
         to do list
       </h1>
       <div className="text-center text-sm text-gray-600 dark:text-gray-300 uppercase tracking-wider">
