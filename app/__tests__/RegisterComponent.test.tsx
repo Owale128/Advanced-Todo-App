4b0d9b790a4df325b02db5310f18b08c801e836a
Owale128 <Christian.buaka@hotmail.com>
2025-12-06 19:59:47 +0100

    test(RegisterComponent): refactor tests for improved structure and clarity

--- app/__tests__/RegisterComponent.test.tsx
+++ app/__tests__/RegisterComponent.test.tsx
@@ -1,52 +1,70 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import axios from "axios";
 import Register from "../registrera/page";
 import { useRouter } from "next/navigation";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 
 jest.mock("next/navigation", () => ({
   useRouter: jest.fn(),
 }));
 
-describe('Register component', () => {
-    const mockPush = jest.fn()
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
-    beforeEach(() => {
-        (useRouter as jest.Mock).mockReturnValue({ push: mockPush})
+describe("Register component", () => {
+  const mockPush = jest.fn();
+
+  beforeEach(() => {
+    (useRouter as jest.Mock).mockReturnValue({ push: mockPush });
+
+    mockedAxios.post.mockResolvedValue({
+      status: 201,
+      data: { message: "User created successfully" },
+    });
+  });
+
+  afterEach(() => {
+    mockPush.mockClear();
+    jest.clearAllMocks();
+  });
+
+  it("should navigate to home on successful registration", async () => {
+    render(<Register />);
+
+    const usernameInput = screen.getByLabelText(/Namn/i);
+    const passwordInput = screen.getByLabelText(/^Lösenord$/i);
+    const confirmPassword = screen.getByLabelText(/^Bekräfta Lösenord$/i);
+    const submitButton = screen.getByRole("button", {
+      name: /Registrera konto/i,
     });
 
-    afterEach(() => {
-        mockPush.mockClear()
-    })
-    
-    it("should navigate to home on successful registration", async () => {
-        render(<Register />)
+    fireEvent.change(usernameInput, { target: { value: "testUser" } });
+    fireEvent.change(passwordInput, { target: { value: "Password123" } });
+    fireEvent.change(confirmPassword, { target: { value: "Password123" } });
+    fireEvent.click(submitButton);
 
-        const usernameInput = screen.getByLabelText(/Namn/i)
-        const passwordInput = screen.getByLabelText(/^Lösenord$/i)
-        const confirmPassword = screen.getByLabelText(/^Bekräfta Lösenord$/i)
-        const submitButton = screen.getByRole("button", {name: /Registrera konto/i})
-        
-        fireEvent.change(usernameInput, { target: { value: "testUser" }})
-        fireEvent.change(passwordInput, { target: { value: "Password123" }})
-        fireEvent.change(confirmPassword, { target: { value: "Password123" }})
-        fireEvent.click(submitButton)
+    await waitFor(() => {
+      expect(mockPush).toHaveBeenCalledWith("/todo-app");
+    });
+  });
 
-        await waitFor(() => {
-            expect(mockPush).toHaveBeenCalledWith("/")
-        })
-    })
-    
-    it("should show error when password dont't match", async () => {
-        render (<Register />)
+  it("should show error when password dont't match", async () => {
+    render(<Register />);
 
-        fireEvent.change(screen.getByLabelText(/Namn/i), {target: {value: "testUser"}})
-        fireEvent.change(screen.getByLabelText(/^Lösenord$/i), {target: {value: "password123"}})
-        fireEvent.change(screen.getByLabelText(/^Bekräfta lösenord$/i), {target: {value: "password12"}})
-        fireEvent.click(screen.getByRole("button", {name: /Registrera konto/i}))
+    fireEvent.change(screen.getByLabelText(/Namn/i), {
+      target: { value: "testUser" },
+    });
+    fireEvent.change(screen.getByLabelText(/^Lösenord$/i), {
+      target: { value: "password123" },
+    });
+    fireEvent.change(screen.getByLabelText(/^Bekräfta lösenord$/i), {
+      target: { value: "password12" },
+    });
+    fireEvent.click(screen.getByRole("button", { name: /Registrera konto/i }));
 
-        await waitFor(() => {
-            expect(screen.getByText(/Passwords do not match/i)).toBeInTheDocument()
-        })
+    await waitFor(() => {
+      expect(screen.getByText(/Passwords do not match/i)).toBeInTheDocument();
+    });
 
-        expect(mockPush).not.toHaveBeenCalled()
-    })
+    expect(mockPush).not.toHaveBeenCalled();
+  });
 });
