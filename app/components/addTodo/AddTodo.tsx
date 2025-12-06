119e644a0489c2a26733f628809832a856a469d5
Owale128 <Christian.buaka@hotmail.com>
2025-12-06 14:24:28 +0100

    feat(AddTodo): implement input validation and priority selection

--- app/components/addTodo/AddTodo.tsx
+++ app/components/addTodo/AddTodo.tsx
@@ -1,23 +1,40 @@
 import { TodosContext } from "@/app/context/TodoContext";
+import { Priority } from "@/app/models/Todo";
 import { FormEvent, useContext, useState } from "react";
 import AddTodoView from "./AddTodoView";
+import { toast } from "sonner";
 
 const AddTodo = () => {
   const [inputValue, setInputValue] = useState("");
+  const [priority, setPriority] = useState<Priority | undefined>(undefined);
   const { add } = useContext(TodosContext);
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
-    add(inputValue);
+
+    if (!inputValue.trim()) {
+      toast.error("Du måste skriva en uppgift!");
+      return;
+    }
+
+    if (!priority) {
+      toast.error("Du måste välja en prioritet!");
+      return;
+    }
+
+    add(inputValue, priority, "General", null);
     setInputValue("");
+    setPriority(undefined);
   };
 
   return (
     <section>
       <AddTodoView
-      handleSubmit={handleSubmit}
+        handleSubmit={handleSubmit}
         inputValue={inputValue}
         setInputValue={setInputValue}
+        priority={priority}
+        setPriority={setPriority}
       />
     </section>
   );
