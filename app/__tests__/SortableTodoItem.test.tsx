0e1d4dd3378af2589a2fc0640744938062653cf4
Owale128 <Christian.buaka@hotmail.com>
2025-12-17 11:21:15 +0100

    feat(SortableTodoItem.test): add testfall to render todo text.

---
+++ app/__tests__/SortableTodoItem.test.tsx
@@ -0,0 +1,52 @@
+import { render, screen } from "@testing-library/react";
+import SortableTodoItem from "../components/sortableTodoItem/SortableTodoItem";
+import { Todo } from "../models/Todo";
+import { DndContext } from "@dnd-kit/core";
+
+jest.mock("framer-motion", () => ({
+  motion: {
+    li: ({ children, ...props }: React.ComponentPropsWithoutRef<'li'>) => <li {...props}>{children}</li>,
+    div: ({ children, ...props }: React.ComponentPropsWithoutRef<'div'>) => <div {...props}>{children}</div>,
+    button: ({ children, ...props }: React.ComponentPropsWithoutRef<'button'>) => <button {...props}>{children}</button>,
+    span: ({ children, ...props }: React.ComponentPropsWithoutRef<'span'>) => <span {...props}>{children}</span>,
+  },
+  AnimatePresence: ({ children }: React.PropsWithChildren) => <>{children}</>,
+}));
+
+describe("SortableTodoItem Component", () => {
+  const mockToggle = jest.fn();
+  const mockRemove = jest.fn();
+  const mockUpdatePriority = jest.fn();
+
+  const mockTodo: Todo = {
+    _id: "test-id-1",
+    text: "Test Todo",
+    done: false,
+    order: 1,
+    priority: "low",
+    category: "work",
+    dueDate: null,
+  };
+
+  const renderTodo = (todo: Todo) => {
+    return render(
+      <DndContext>
+        <SortableTodoItem
+          todo={todo}
+          toggle={mockToggle}
+          remove={mockRemove}
+          updatePriority={mockUpdatePriority}
+        />
+      </DndContext>
+    );
+  };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should render todo text", () => {
+    renderTodo(mockTodo);
+    expect(screen.getByText("Test Todo")).toBeInTheDocument();
+  });
+});
